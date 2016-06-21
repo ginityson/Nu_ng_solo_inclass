82aecc36c416a8466031bb27be67cbc9e2381d34
GINI TYSON <ginityson@yahoo.com>
2016-06-21 16:08:35 -0500

    Table Names and Capacity Working

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -2,11 +2,31 @@ console.log('here we go again script.js sourced');
 
 var myApp = angular.module( "myApp", [] );
 
-myApp.controller( "testController", [ "$scope", function( $scope ){
-  $scope.allInput = [];
-  $scope.getTestInput = function(){
-    console.log( 'input captured: ' + $scope.testBinder );
-      $scope.allInput.push( $scope.testBinder );
-      console.log('all input: ' + $scope.allInput);
+myApp.controller( "chezTablesController", [ "$scope", function( $scope ){
+
+  $scope.allTables = [];
+
+  $scope.capacity = [
+    {capacity: 'two'},
+    {capacity: 'four'},
+    {capacity: 'six'},
+    {capacity: 'eight'}
+  ];
+  $scope.createTable = function(){
+      $scope.newTable = {
+        name: $scope.tableNameBinder,
+        capacity: $scope.capacityIn
+      };
+      $scope.allTables.push( $scope.newTable );
+      console.log($scope.newTable);
+      console.log('all tables: ' + $scope.allTables);
+
+      var outputText = '';
+      for(var i=0; i<$scope.allTables.length; i++){
+        console.log($scope.allTables);
+        outputText +=  $scope.allTables[i].name + $scope.allTables[i].capacityIn;
+
+      }
+
   };
 }]);//end of controller
